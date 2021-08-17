fc0914380effad57da59a074fa7496e2deb0a899
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2021-08-17 15:18:31 -0300

    Remove nav link

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -8,7 +8,6 @@
       <a href="/" class="btn btn-outline-primary mr-1">Home</a>
       <a href="http://localhost:3000/api/users" class="btn btn-primary mr-1">Server API</a>
       <a href="https://github.com/leonardogbxv" class="btn btn-secondary mr-1">Github</a>
-      <a href="https://www.operand.com.br/" class="btn btn-outline-warning float-right text-monospace">Op</a>
     </nav>
   </div>
 </template>
